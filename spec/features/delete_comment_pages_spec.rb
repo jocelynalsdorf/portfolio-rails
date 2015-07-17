0e3d763f9f0de409f1e13d8fcc6e476445e2965f
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-17 09:41:07 -0700

    spec passing with js:true for delete a comment

--- spec/features/delete_comment_pages_spec.rb
+++ spec/features/delete_comment_pages_spec.rb
@@ -4,22 +4,21 @@ Warden.test_mode!
 
 
 describe "the delete a comment process" do
-  it "delete a comment from a post" do  
+  it "delete a comment from a post", js: true  do  
     visit skills_path
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    post = FactoryGirl.create(:post)
     click_link 'Blog'
     click_link 'Add Post'
-    fill_in 'Title', :with => post.title
-    fill_in 'Body', :with => post.body
+    fill_in 'Title', :with => "my post"
+    fill_in 'Body', :with => "my body"
     click_on 'Create Post'
-    comment = FactoryGirl.create(:comment)
-    fill_in 'Content', :with => comment.content
+    click_on 'Add comment'
+    fill_in 'Content', :with => "a comment"
     click_on 'Create Comment'
-    click_link comment.content
+    click_link 'a comment'
     click_link 'Delete'
     expect(page).to have_content 'Admin Page'
   end
-
 end
+
