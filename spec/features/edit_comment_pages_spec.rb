891dbf34832e4e8948642e275833f4606d0d292c
jocelynalsdorf <jfrodsla@gmail.com>
2015-07-17 10:35:12 -0700

    all 38 tests passing with 98.26% coverage

--- spec/features/edit_comment_pages_spec.rb
+++ spec/features/edit_comment_pages_spec.rb
@@ -4,44 +4,43 @@ Warden.test_mode!
 
 
 describe "the edit a comment process" do
-  it "edit a new comment to a post" do  
+  it "edit a new comment to a post", js: true do  
     visit skills_path
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    post = FactoryGirl.create(:post)
     click_link 'Blog'
     click_link 'Add Post'
-    fill_in 'Title', :with => post.title
-    fill_in 'Body', :with => post.body
+    fill_in 'Title', :with => "my post"
+    fill_in 'Body', :with => "my body"
     click_on 'Create Post'
-    comment = FactoryGirl.create(:comment)
-    fill_in 'Content', :with => comment.content
+    click_on 'Add comment'
+    fill_in 'Content', :with => "a comment"
     click_on 'Create Comment'
-    click_link comment.content
-    click_link 'Edit'
+    click_link 'a comment'
+    click_link 'Edit Comment'
     fill_in 'Content', :with => 'new comment'
     click_on 'Update Comment'
     expect(page).to have_content 'new comment'
   end
 
-  it "gives an error when no data is entered into form fields" do
+  it "gives an error when no data is entered into form fields", js: true do
     visit skills_path
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
-    post = FactoryGirl.create(:post)
     click_link 'Blog'
     click_link 'Add Post'
-    fill_in 'Title', :with => post.title
-    fill_in 'Body', :with => post.body
+    fill_in 'Title', :with => "my post"
+    fill_in 'Body', :with => "my body"
     click_on 'Create Post'
-    comment = FactoryGirl.create(:comment)
-    fill_in 'Content', :with => comment.content
+    click_on 'Add comment'
+    fill_in 'Content', :with => "a comment"
     click_on 'Create Comment'
-    click_link comment.content
-    click_link 'Edit'
-    fill_in 'Content', :with => ''
+    click_on 'a comment'
+    click_link 'Edit Comment'
+    fill_in "comment[content]", :with => ''
     click_on 'Update Comment'
-    expect(page).to have_content 'What do you think you are doing??!!!'
-  end
+    expect(page).to have_content 'What do you think you are doing'
+   end
 
 end
+
